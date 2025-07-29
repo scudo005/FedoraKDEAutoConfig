5cf6af343377cf920e937d37b72759aed103f192
scudo005 <96631443+scudo005@users.noreply.github.com>
2025-07-29 23:21:52 +0200

    bump to Fedora 41 with tested functionality

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # FedoraKDEAutoConfig
-Script to automatically configure Fedora (39, for now) KDE to automatically add RPM Fusion and Flathub repositories.
+Script to automatically configure Fedora (41) KDE to automatically add RPM Fusion and Flathub repositories.
 
-This script installs both free **and non-free** repositories from RPM Fusion. It also installs neofetch (because we all love it and Fedora doesn't provide it automatically) and Wine from WineHQ (stable).
+This script installs both free **and non-free** repositories from RPM Fusion. It also installs `fastfetch` (because we all love it and Fedora doesn't provide it automatically), Wine, and a bunch of other things I want in my workflow.
 
 I made this to simplify *my* process in installing Fedora, so feel free to fork this project and add the changes you need.
+
+Notes: 
+- if it doesn't work, try invoking `dos2unix` on the script file, as GitHub may save it as a Windows-formatted text file, and give it executability permission with `chmod +x`.
--- configure.sh
+++ configure.sh
@@ -1,13 +1,22 @@
-sudo -s
-dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
-dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-dnf update -y
-dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/39/winehq.repo
-dnf install winehq-stable neofetch -y
-# dnf install vlc python-vlc
-# dnf remove dragon elisa-player
-dnf autoremove -y
-exit
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf update -y
+sudo dnf install wine fastfetch vlc python-vlc gcc g++ make dotnet-sdk-9.0 kdenlive gimp -y
+sudo dnf remove dragon elisa-player -y
+sudo dnf autoremove -y
+IS_INTEL_HD_IGPU=$(lspci | grep "Intel Corporation HD")
+IS_INTEL_UHD_IGPU=$(lspci | grep "Intel Corporation UHD")
+if [[ $IS_INTEL_HD_IGPU == *"Intel Corporation HD"* || $IS_INTEL_UHD_IGPU == *"Intel Corporation UHD"* ]]; then
+    sudo dnf install intel-media-driver -y
+fi
+unset IS_INTEL_HD_IGPU
+unset IS_INTEL_UHD_IGPU
+sudo dnf install ffmpeg -y --allowerasing # nonfree stuff like Intel QSV
+# MiKTeX stuff
+sudo rpm --import "https://miktex.org/download/key"
+sudo curl -L -o /etc/yum.repos.d/miktex.repo https://miktex.org/download/fedora/41/miktex.repo
+sudo dnf update -y
+sudo dnf install miktex -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak update -y
-# flatpak install com.vscodium.codium com.github.Eloston.UngoogledChromium com.valvesoftware.Steam -y
+flatpak install com.vscodium.codium io.github.ungoogled_software.ungoogled_chromium com.valvesoftware.Steam org.keepassxc.KeePassXC org.mozilla.Thunderbird fr.handbrake.ghb fr.handbrake.ghb org.signal.Signal -y
